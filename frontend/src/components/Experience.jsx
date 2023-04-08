aba331866493cd242977d3ccd285e9c62c1c566e
Jiayuan Hong <einsgatehong@gmail.com>
2023-04-08 00:00:41 -0500

    first for feedbacks and experience

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { SectionWrapper } from '../loc'
-
+import { styles } from '../styles'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div>
+      <div className={styles.heroHeadText}>
+        Experience
+      </div>
+      This will change later
+      <li>This app greatly helped me generate my code -- Johnson</li>
+      <li>I used this app to improve my efficiency of coding -- Andrew</li>
+      <li>This is a very helpful app -- Wilson</li>
+    </div>
   )
 }
 
--- frontend/src/components/Feedbacks.jsx
+++ frontend/src/components/Feedbacks.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { SectionWrapper } from '../loc'
+import { styles } from '../styles'
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div>
+      <div className={styles.heroHeadText}>
+        Feedback
+      </div>
+      <li>This app greatly helped me generate my code -- Johnson</li>
+      <li>I used this app to improve my efficiency of coding -- Andrew</li>
+      <li>This is a very helpful app -- Wilson</li>
+    </div>
   )
 }
 
